5dfe24f53d2d1e3a8757097d2dd641ab00bfd60b
honghuajun <erfu81856@gmail.com>
2024-11-21 11:24:32 +0800

    fucking solve

--- widget/Counter.tsx
+++ widget/Counter.tsx
@@ -2,14 +2,19 @@ import { Variable, bind } from "astal"
 
 export default function Counter() {
     const count = Variable(0)
-    let num:String = count.get().toString()
     function increment() {
         count.set(count.get() + 1)
+        console.log(count.get().toString())
+        let num:String = count.get().toString()
+        return <box>
+            {num.toString()}
+        </box>
     }
+    
     return <box>
-        <label label={bind(count).as(num => num.toString())} />
+        <label label={bind(count).as(num => num.toString())} as/>
         <button onClicked={increment}>
-            <label label={num}></label>
+            {bind(count).as(num => num.toString())}
         </button>
     </box>
 }
