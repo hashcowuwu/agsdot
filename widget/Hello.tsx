56dcfc34d06917d1331eca6182232eb2e9db76fc
honghuajun <erfu81856@gmail.com>
2024-11-22 13:10:59 +0800

    fix

--- widget/Hello.tsx
+++ widget/Hello.tsx
@@ -4,13 +4,13 @@ import { execAsync } from "astal"
 export default function Hello() {
 
   function changeWallpaper() {
-execAsync(["bash", "-c", "/home/hhj/code/agsdot/lib/changeWallpaper.sh"])
+    execAsync(["bash", "-c", "/home/hhj/code/agsdot/lib/changeWallpaper.sh"])
     .then((out) => console.log(out))
     .catch((err) => console.error(err))
   }
   return <button
-    onClicked={changeWallpaper}
-    halign={Gtk.Align.CENTER} >
-    ♥️
+  onClicked={changeWallpaper}
+  halign={Gtk.Align.CENTER} >
+  ♥️
   </button>
 }
