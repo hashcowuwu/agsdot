6846ef9a44d9f215f7285c040a7f7ef14cf7d119
honghuajun <erfu81856@gmail.com>
2024-11-22 13:14:34 +0800

    fix dic

--- lib/changeWallpaper.sh
+++ lib/changeWallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 num=$RANDOM
-urk='/home/hhj/wallpaper'
+urk='/home/'$USER'/wallpaper'
 arr=(echo $urk/*)
 sum=`ls -l $urk |grep "^-"|wc -l`
 swww img  ${arr[($num%$sum)+1]} 	--transition-fps 60 --transition-type center 
