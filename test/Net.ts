72dc0694395ec6bd3b96c6ce9c2a5890b12043e9
hashcowuwu <3189506476@qq.com>
2024-11-26 18:25:27 +0800

    add Fuck Test

--- test/Net.ts
+++ test/Net.ts
@@ -6,5 +6,22 @@ print(network.state)
 print(network.connectivity)
 print(network.primary)
 print(network.wired.speed)
-print(network.wired.device.name)
-print(network.wired.speed)
+
+//print(network.client.devices)
+print(network.client.hostname)
+print(network.client.state)
+print(network.client.version)
+print(network.wired.get_icon_name())
+print(network.wired.get_state())
+
+//print(network.client.devices)
+
+
+print("================")
+
+
+const tmp = Network.device_state_to_string()
+console.log(tmp);
+
+
+//print(network.client.devices)
